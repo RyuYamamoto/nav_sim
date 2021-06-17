c7b44798ec3038a3414bb049b3ff1c6fca99a795
RyuYamamoto <ryu.yamamoto@tier4.jp>
2021-06-17 23:30:47 +0900

    add overload func

--- include/nav_sim/nav_sim.h
+++ include/nav_sim/nav_sim.h
@@ -30,6 +30,22 @@ struct State
   double yaw_;
   State() : x_(0.0), y_(0.0), yaw_(0.0) {}
   State(double x, double y, double yaw) : x_(x), y_(y), yaw_(yaw) {}
+  State operator+(State pose_a)
+  {
+    State pose_b;
+    pose_b.x_ = this->x_ + pose_a.x_;
+    pose_b.y_ = this->y_ + pose_a.y_;
+    pose_b.yaw_ = this->yaw_ + pose_a.yaw_;
+    return pose_b;
+  }
+  State operator-(State pose_a)
+  {
+    State pose_b;
+    pose_b.x_ = this->x_ - pose_a.x_;
+    pose_b.y_ = this->y_ - pose_a.y_;
+    pose_b.yaw_ = this->yaw_ - pose_a.yaw_;
+    return pose_b;
+  }
 };
 
 struct Landmark
